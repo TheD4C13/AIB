6fadf6ae0e84cfe3e422a4ddfefc45e8faf04b4b
TheD4C13 <135219087+TheD4C13@users.noreply.github.com>
2023-06-09 10:29:04 +0700

    app.py

--- app.py
+++ app.py
@@ -105,6 +105,8 @@ def main():
         # elif(valuesimg == 2):
         #     st.image(image3, caption='picture 3')
         #     st.write("ผลลัพท์การตรวจสอบ")
+    elif option == "Video": 
+        videoInput(deviceoption, datasrc)
 
        
 
